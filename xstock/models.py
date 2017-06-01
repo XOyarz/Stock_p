4613e46eb26ce7baf416620e550d23238d88636c
XOyarz <xavier982@hotmai.com>
2017-06-01 16:41:59 +0200

    Model created

--- xstock/models.py
+++ xstock/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Portfolio(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(blank=True, max_digits=15, decimal_places=2)
+    symbol = models.TextField(max_length=4)
+    shares = models.IntegerField(blank=True)
+    total_price = models.DecimalField(blank=True, max_digits=15, decimal_places=2)
+    date = models.DateField(null=False)
+    user = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.name
