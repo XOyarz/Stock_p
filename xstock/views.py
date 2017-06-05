1ca4751615e74afc335c38c0b37503c81c7b717f
XOyarz <xavier982@hotmail.com>
2017-06-05 13:12:05 +0200

    Add files via upload

--- xstock/views.py
+++ xstock/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from xstock.forms import StockForm
 from xstock.helpers import *
+from xstock.models import Portfolio
+import datetime
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def index(request):
-    print()
+    print(request.user)
     return render(request, 'xstock/index.html')
 
 def about(request):
@@ -28,10 +32,24 @@ def buy(request):
             #shares = quoted_stock['shares']
             shares = data['share_amount']
             total_price = price * int(shares)
-            user = user.username
-            context_dict = {'name':name, 'price':price, 'symbol':symbol, 'shares':shares, 'total_price':total_price}
+            user = str(request.user)
+            context_dict = {'name':name, 'price':price, 'symbol':symbol, 'shares':shares, 'total_price':total_price, 'user':user}
+            purchase(context_dict, user)
             return render(request, 'xstock/buy2.html', context=context_dict)
 
     elif request.method == 'GET':
         form = StockForm()
         return render(request, 'xstock/buy.html', {'form':form})
+
+
+def purchase(stock_info, user):
+    p = Portfolio(
+        name=stock_info["name"],
+        price=stock_info["price"],
+        symbol=stock_info["symbol"],
+        shares=stock_info["shares"],
+        total_price=stock_info["total_price"],
+        date=datetime.datetime.now(),
+        user=user
+    )
+    p.save()
