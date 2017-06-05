891c91caf9bc44f4ce1efd110d26314049152c0a
XOyarz <xavier982@hotmail.com>
2017-06-05 14:01:54 +0200

    Add files via upload

--- xstock/admin.py
+++ xstock/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from xstock.models import Portfolio
 
+class PortfolioModelAdmin(admin.ModelAdmin):
+    fields = ['name', 'price', 'symbol', 'shares', 'user', 'action']
+
+    list_display = ('name', 'price', 'symbol', 'shares', 'user', 'action')
+    search_fields = ('name', 'symbol', 'action')
+
+    class Meta:
+        model = Portfolio
+
+
 # Register your models here.
-admin.site.register(Portfolio)
+admin.site.register(Portfolio, PortfolioModelAdmin)
